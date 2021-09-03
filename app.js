18d80782cfa70db684c59188beb5c84a7ee4254f
achref-abidi <abidi29achref@gmail.com>
2021-09-03 15:50:31 +0100

    adding website

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const app = require("express")();
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.get("/", (req, res) => {
-    res.send("hello achref");
+    res.sendFile(__dirname + "/views/index.html")
 });
 
+app.use(express.static(__dirname + "/public/style"));
+
 app.listen(PORT, () => {
-    console.log(`App up at port ${PORT}`);
+    console.log(`App listening at http://localhost:${PORT}`);
 })
