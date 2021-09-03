cd4aa99ab24aae193305da1160bb701b0e96404b
achref-abidi <abidi29achref@gmail.com>
2021-09-03 14:21:22 +0100

    The first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = require("express")();
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/", (req, res) => {
+    res.send("hello");
+});
+
+app.listen(PORT, () => {
+    console.log(`App up at port ${PORT}`);
+})
